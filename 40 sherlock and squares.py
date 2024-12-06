b23aad082d293368c69b7ce1c0836da494c4cfe7
Ankan9330 <139869083+Ankan9330@users.noreply.github.com>
2024-12-06 16:38:38 +0530

    Add files via upload

---
+++ 40 sherlock and squares.py
@@ -0,0 +1,63 @@
+'''Watson likes to challenge Sherlock's math ability. He will provide a starting and ending value that describe a range of integers, inclusive of the endpoints. Sherlock must determine the number of square integers within that range.
+
+Note: A square integer is an integer which is the square of an integer, e.g. .
+
+Example
+
+
+There are three square integers in the range:  and . Return .
+
+Function Description
+
+Complete the squares function in the editor below. It should return an integer representing the number of square integers in the inclusive range from  to .
+
+squares has the following parameter(s):
+
+int a: the lower range boundary
+int b: the upper range boundary
+Returns
+
+int: the number of square integers in the range
+Input Format
+
+The first line contains , the number of test cases.
+Each of the next  lines contains two space-separated integers,  and , the starting and ending integers in the ranges.
+
+Constraints
+
+
+
+Sample Input
+
+2
+3 9
+17 24
+Sample Output
+
+2
+0
+Explanation
+
+Test Case #00: In range ,  and  are the two square integers.
+Test Case #01: In range , there are no square integers.'''
+import os
+import math
+
+def squares(a, b):
+    return math.floor(math.sqrt(b)) - math.ceil(math.sqrt(a)) + 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        ab = input().split()
+        a = int(ab[0])
+        b = int(ab[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
