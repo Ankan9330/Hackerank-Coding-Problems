7f68e486c5ed8907b55e0e148e1caa5af30855b9
Ankan9330 <139869083+Ankan9330@users.noreply.github.com>
2024-12-06 16:37:44 +0530

    Add files via upload

---
+++ 37 find digits.py
@@ -0,0 +1,72 @@
+'''An integer  is a divisor of an integer  if the remainder of .
+
+Given an integer, for each digit that makes up the integer determine whether it is a divisor. Count the number of divisors occurring within the integer.
+
+Example
+
+Check whether ,  and  are divisors of . All 3 numbers divide evenly into  so return .
+
+
+Check whether , , and  are divisors of . All 3 numbers divide evenly into  so return .
+
+
+Check whether  and  are divisors of .  is, but  is not. Return .
+
+Function Description
+
+Complete the findDigits function in the editor below.
+
+findDigits has the following parameter(s):
+
+int n: the value to analyze
+Returns
+
+int: the number of digits in  that are divisors of 
+Input Format
+
+The first line is an integer, , the number of test cases.
+The  subsequent lines each contain an integer, .
+
+Constraints
+
+
+
+Sample Input
+
+2
+12
+1012
+Sample Output
+
+2
+3
+Explanation
+
+The number  is broken into two digits,  and . When  is divided by either of those two digits, the remainder is  so they are both divisors.
+
+The number  is broken into four digits, , , , and .  is evenly divisible by its digits , , and , but it is not divisible by  as division by zero is undefined.'''
+import os
+
+def findDigits(n):
+    answer = 0
+
+    for s in str(n):
+        i = int(s)
+        if i != 0 and n % i == 0:
+            answer += 1
+
+    return answer
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
